89355189caaaeb272786e1d8accaefb27cb6070e
Pappu Kumar <digitalpappu101@gmail.com>
2022-07-19 00:11:13 +0530

    d

--- test1/src/Context/AuthContext.jsx
+++ test1/src/Context/AuthContext.jsx
@@ -2,15 +2,36 @@ import React from 'react'
 import {createContext} from 'react'
 import axios from 'axios'
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 export const AuthContext=createContext();
+
 export const AuthContextProvider = ({children}) => {
-    
+   
     const[token,setToken]=useState("");
-    const handleLogin=()=>{
-          axios({
-            mathod:"post",
-            url:'https://reqres.in/api/login'
-          }).then((res)=>(setToken(res.data)));
+
+    useEffect(() =>{
+
+
+    })
+    const handleLogin=(body)=>{
+      console.log(body)
+      fetch("https://reqres.in/api/login",
+      {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify({
+            email:body.email,
+            password:body.pass
+          })
+      })
+      .then( (res)=>res.json())
+      .then((res)=>{setToken(res.token)})
+      .catch(function(res){ console.log(res) })
+     
+
     }
 
   return (
